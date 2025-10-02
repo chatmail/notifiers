f4ddb2b6ee742ef87ed92cbf1a34c49ff58f2fa7
d2weber <29163905+d2weber@users.noreply.github.com>
2025-10-02 06:50:13 +0200

    feat: add support for ubuntu touch
    Documentation for reference:
    https://docs.ubports.com/en/latest/appdev/guides/pushnotifications.html

--- src/notifier.rs
+++ src/notifier.rs
@@ -85,7 +85,7 @@ async fn wakeup(
     let device_token: NotificationToken = key_device_token.as_str().parse()?;
 
     let (client, device_token) = match device_token {
-        NotificationToken::Fcm { .. } => {
+        NotificationToken::Fcm { .. } | NotificationToken::UBports(..) => {
             // Only APNS tokens can be registered for periodic notifications.
             info!("Removing FCM token {key_device_token}");
             schedule
